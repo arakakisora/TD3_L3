3b12835fa214124347b5881752fbb3ffaff9a41e
kitajima-hayato <k023g0057@g.neec.ac.jp>
2025-02-28 06:16:40 +0900

    a

--- engine/3d/Object3D.h
+++ engine/3d/Object3D.h
@@ -33,7 +33,7 @@ public:
 	//transrat
 	void SetTransform(const Transform& transform) { this->transform = transform; }
 	Transform GetTransform() { return transform; }
-
+	 
 	//スケール
 	void SetScale(const Vector3& scale) { transform.scale = scale; }
 	//回転
