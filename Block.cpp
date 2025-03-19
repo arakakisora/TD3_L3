c30376fc763c552c9050e487d6b08e8f49cc609d
kitajima-hayato <k023g0057@g.neec.ac.jp>
2025-03-19 09:23:12 +0900

    ブロッククラスの初期化と更新処理のコメント追加
    `Block::Initialize`、`Block::Update`、`Block::Draw`、および `Block::Finalize` メソッドにコメントを追加し、特定のマップチップタイプに対する処理や将来的な拡張の参考となる情報を提供しました。これにより、コードの可読性と保守性が向上します。

--- Block.cpp
+++ Block.cpp
@@ -21,6 +21,12 @@ void Block::Initialize(MapChipType type, const Vector3& position) {
 
 	switch ((type))
 	{
+	//case MapChipType::ここにマップチップタイプ:		// コピー不可能 No.2
+	//	// モデル指定
+	//	object3D->SetModel("使用したいモデル.obj");
+		//　もし、ブロックようにクラスを作ったのであればここに初期化
+		// 
+	//	break;
 	case MapChipType::kNCopyBlock:		// コピー不可能 No.2
 		// モデル指定
 		object3D->SetModel("axis.obj");
@@ -39,8 +45,16 @@ void Block::Initialize(MapChipType type, const Vector3& position) {
 }
 
 void Block::Update() {
+	/*else if (MapChipType::マップチップタイプ == type) {
+	* モデルの更新なのでこれは絶対に必要
+		object3D->Update();
+		作ったブロックの更新処理
+		ブロッククラス内で作ればそれを呼ぶだけでいい
+		追加する場合はkNCopyBlockの部分を参考に
+	}*/
 	if (MapChipType::kCopyBlock == type) {
 		object3D->Update();
+
 	} else if (MapChipType::kNCopyBlock == type) {
 		object3D->Update();
 	}
@@ -48,6 +62,7 @@ void Block::Update() {
 
 
 void Block::Draw() {
+	// Drawはelse ifを追加してDrawかくだけ
 	if (MapChipType::kCopyBlock == type) {
 		object3D->Draw();
 	} else if (MapChipType::kNCopyBlock == type) {
@@ -56,6 +71,8 @@ void Block::Draw() {
 }
 
 void Block::Finalize() {
+	// 別のクラスを作った場合はそのクラスの終了処理を呼ぶ
+	// モデルの解放はこの変数しか使用しないようになっているので追加でかく必要は無し
 	if (object3D) {
 		delete object3D;
 		object3D = nullptr;
