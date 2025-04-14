a5901ab2febe223646f4bd9687edcd7318679100
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-04-14 11:36:52 +0900

    クリアシーン追加

--- engine/scene/GameClearScene.h
+++ engine/scene/GameClearScene.h
@@ -1,6 +1,14 @@
 #pragma once
 #include "BaseScene.h"
-class GameClearScene:public BaseScene
+#include "Camera.h"	
+#include "Model.h"
+#include"Sprite.h"
+#include "Object3D.h"
+#include "Audio.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+class GameClearScene :public BaseScene
 {
 
 public:
@@ -21,7 +29,56 @@ public:
 	/// </summary>
 	void Draw()override;
 
+
+	void EasingMove();
+
+	void StartJump();
+
+
+	float EaseOutElastic(float t, float b, float c, float d, float amplitudeScale = 0.3f) {
+		if (t == 0) return b;
+		if ((t /= d) == 1) return b + c;
+		float p = d * 0.1f; // 伸縮の周期
+		float a = c * amplitudeScale; // 振幅をスケールダウン
+		float s = p / 4.0f;
+		return a * powf(2.0f, -10.0f * t) * sinf((t * d - s) * (2.0f * static_cast<float>(M_PI)) / p) + c + b;
+	}
+
+	float EaseOutBounce(float time, float start, float change, float duration) {
+		float t = time / duration;
+		if (t < (1 / 2.75f)) {
+			return change * (7.5625f * t * t) + start;
+		} else if (t < (2 / 2.75f)) {
+			t -= (1.5f / 2.75f);
+			return change * (7.5625f * t * t + 0.75f) + start;
+		} else if (t < (2.5 / 2.75)) {
+			t -= (2.25f / 2.75f);
+			return change * (7.5625f * t * t + 0.9375f) + start;
+		} else {
+			t -= (2.625f / 2.75f);
+			return change * (7.5625f * t * t + 0.984375f) + start;
+		}
+	}
+
 private:
+	// テキストオブジェクト
+	std::vector<std::unique_ptr<Object3D>> Cleartext_;
+	// 最大テキスト数
+	uint32_t MaxtextIndex_ = 3;
 
+	float elapsedTime = 0.0f; // 経過時間
+	const float animationDuration = 1.0f; // アニメーション時間
+	bool allObjectsFinished = false; // すべてのオブジェクトがアニメーションを終わらせたかを追跡
+
+	float deltaTime = 0.01f;
+
+	float objecttime_[3];
+	float startDelay[3];
+	bool fige = false;
+	// 速度を格納するための配列（各オブジェクトの速度を管理）
+	std::vector<Vector3> velocity_;
+
+	float elapsedTime_;
+	float	elapsedFrames_ = 0;
+	float jumpEndTime_[3] = { 0.0f };
 };
-
