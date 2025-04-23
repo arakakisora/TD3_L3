2f8babf9d29aeb23e7e4bf1277fa2e42f9b77692
taohdghas <143993457+taohdghas@users.noreply.github.com>
2025-04-23 22:20:21 +0900

    マージ後{}のつけ忘れがあったので修正

--- engine/scene/GamePlayScene.cpp
+++ engine/scene/GamePlayScene.cpp
@@ -484,7 +484,8 @@ void GamePlayScene::Update()
 		}
 		//mode切り替え
 		photoCamera->SetcameraMode(player->GetcamerMode());
-	}
+
+	
 	
 	// ポーズ
 	pauseMenu->Update();
@@ -667,8 +668,8 @@ void GamePlayScene::Update()
 	}
 
 #endif // _DEBUG
-
 }
+
 
 void GamePlayScene::Draw()
 {
