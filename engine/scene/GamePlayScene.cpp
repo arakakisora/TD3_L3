7b03c3e5e8ba1d68510a3f248673a74af9354b1b
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-05-19 12:56:32 +0900

    パーティクル処理の位置移動

--- engine/scene/GamePlayScene.cpp
+++ engine/scene/GamePlayScene.cpp
@@ -363,6 +363,7 @@ void GamePlayScene::Initialize()
 		1,
 		"Player"
 	);
+	playeroffset = { 0.0f,0.0f,0.0f };
 }
 
 void GamePlayScene::Finalize()
@@ -407,8 +408,38 @@ void GamePlayScene::Update()
 		map->Update();
 		//プレイヤーの更新
 		player->Update();
+		
+		// プレイヤーが右に移動中
+		if (player->GetPrayerMoveRight()) {
+			playeroffset = { -0.3f,0.0f,0.0f };
+			playeremitter_->SetPosition(player->GetTranslate() + playeroffset);
+			// 左方向に設定
+			playeremitter_->SetisRight(false);
+			// プレイヤーのパーティクルを発生させる
+			playeremitter_->PlayerEmit();
+		}
 
+		// プレイヤーが左に移動中
+		if (player->GetPrayerMoveLeft()) {
+			playeroffset = { 0.3f,0.0f,0.0f };
+			playeremitter_->SetPosition(player->GetTranslate() + playeroffset);
+			// 右方向に設定
+			playeremitter_->SetisRight(true);
+			// プレイヤーのパーティクルを発生させる
+			playeremitter_->PlayerEmit();
+		} 
+
+		playeremitter_->SetPosition(player->GetTranslate() + playeroffset);
+		// パーティクルの更新
 		playeremitter_->Update();
+
+		//チュートリアル表示制御map2
+		if (SceneManager::GetInstance()->GetStageIndex() == 1) {
+			if (!tutorial9) {
+				Tutorialtext9->SetIsTutorialActive(true);
+				tutorial9 = true;
+			}
+		}
 
 		if (player->GetCameraMode()) {
 			// カメラモード時パーティクル解除
@@ -522,35 +553,6 @@ void GamePlayScene::Update()
 	} else {
 		player->SetPrayerMoveRight(false);
 		player->SetPrayerMoveLeft(false);
-	}
-
-	// プレイヤー用のパーティクルの位置を常に更新
-	Vector3 pos = player->GetTranslate();
-
-	// 右に移動中
-	if (player->GetPrayerMoveRight()) {
-		// 左方向に設定
-		playeremitter_->SetisRight(false);
-		Vector3 offset = { -0.3f,0.0f,0.0f };
-		playeremitter_->SetPosition(pos + offset);
-		playeremitter_->PlayerEmit();
-	}
-
-	// 左に移動中
-	if (player->GetPrayerMoveLeft()) {
-		// 右方向に設定
-		playeremitter_->SetisRight(true);
-		Vector3 offset = { 0.3f,0.0f,0.0f };
-		playeremitter_->SetPosition(pos + offset);
-		playeremitter_->PlayerEmit();
-	}
-
-	//チュートリアル表示制御map2
-	if (SceneManager::GetInstance()->GetStageIndex() == 1) {
-		if (!tutorial9) {
-			Tutorialtext9->SetIsTutorialActive(true);
-			tutorial9 = true;
-		}
 	}
 
 	if (Tutorialtext1->GetIsTutorialActive()) Tutorialtext1->Update();
